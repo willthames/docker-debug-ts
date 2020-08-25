86c04f8abcbe63fc3d8533858313f819af388d58
Will Thames <will@thames.id.au>
2020-08-25 11:17:29 +1000

    Add nginx sidecar

--- Dockerfile
+++ Dockerfile
@@ -1,15 +1,19 @@
-FROM node:12.18.3 AS builder
+FROM node:12.18.3 AS installer
 WORKDIR /build
 COPY yarn.lock package.json ./
 RUN yarn install --frozen-lockfile
+
+FROM node:12.18.3 AS builder
+WORKDIR /build
+COPY --from=installer /build/node_modules ./node_modules
 COPY ./ .
 RUN yarn build
 
 FROM node:12.18.3
 WORKDIR /app
-COPY --from=builder /build/dist ./dist
 COPY package.json ./
-COPY --from=builder /build/node_modules ./node_modules
+COPY --from=installer /build/node_modules ./node_modules
 COPY static ./static
 COPY src/views ./src/views
+COPY --from=builder /build/dist ./dist
 CMD ["yarn", "start"]
--- src/middleware/tracing.ts
+++ src/middleware/tracing.ts
@@ -1,16 +1,28 @@
 import Koa from 'koa';
-import { propagation, Tracer } from '@opentelemetry/api';
+import { propagation, Tracer, Span } from '@opentelemetry/api';
+import { getExtractedSpanContext } from '@opentelemetry/core';
 
 import { factory } from '../logging';
 
 const log = factory.getLogger('tracing');
 
-/**
- * @typedef {Object} MiddlewareOptions
- * @property {Object} tracer
- * @property {string} serviceName
- * @property {number} port
- */
+const recordResponse = (ctx: Koa.Context, span: Span) => {
+  span.setAttribute('http.status_code', ctx.response.status);
+  span.setAttribute('http.status_line', ctx.response.message);
+  if (ctx.response.body) {
+    span.setAttribute('http.bytes', ctx.response.body.length);
+  }
+  if (ctx.request.get('User-Agent')) {
+    span.setAttribute('http.user_agent', ctx.request.get('User-Agent'));
+  }
+  if (ctx.request.get('Referer')) {
+    span.setAttribute('http.referrer', ctx.request.get('Referer'));
+  }
+};
+
+const recordError = (span: Span) => {
+  span.setAttribute('error', true);
+};
 
 /**
  * @param {MiddlewareOptions}
@@ -25,49 +37,41 @@ export function koaMiddleware(tracer: Tracer) {
    */
   return async function zipkinKoaMiddleware(ctx: Koa.Context, next: Koa.Next) {
     ctx.tracer = tracer;
-    let ourContext;
-    try {
-      ourContext = propagation.extract(ctx.request.headers);
-    } catch (error) {
-      log.error("Couldn't get propagation: ", error);
-      next();
-      return;
-    }
-    const span = tracer.startSpan(
-      `${ctx.request.method} ${ctx.request.path}`,
-      {
-        kind: 1,
-        attributes: {
-          'http.path': ctx.request.path,
-          'http.method': ctx.request.method,
+    const ourContext = propagation.extract(ctx.request.headers);
+
+    let span: Span | undefined;
+    if (getExtractedSpanContext(ourContext)) {
+      span = tracer.startSpan(
+        `${ctx.request.method} ${ctx.request.path}`,
+        {
+          kind: 1,
+          attributes: {
+            'http.path': ctx.request.path,
+            'http.method': ctx.request.method,
+            'http.url': ctx.request.url,
+          },
         },
-      },
-      ourContext,
-    );
-    ctx.span = span;
-    log.info(`New span: ${span.context().traceId} ${span.context().spanId}`);
-    const recordResponse = () => {
-      span.setAttribute('http.status', ctx.response.status);
-      if (ctx.response.body) {
-        span.setAttribute('http.bytes', ctx.response.body.length);
+        ourContext,
+      );
+      if (ctx.request.headers['X-Request-Id']) {
+        span.setAttribute('request_id', 'X-Request-Id');
       }
-      if (ctx.request.get('User-Agent')) {
-        span.setAttribute('http.user_agent', ctx.request.get('User-Agent'));
-      }
-      if (ctx.request.get('Referer')) {
-        span.setAttribute('http.referrer', ctx.request.get('Referer'));
-      }
-      span.end();
-    };
-    const recordError = () => {
-      span.setAttribute('error', true);
-    };
+      ctx.span = span;
+      log.info(`New span: ${span.context().traceId} ${span.context().spanId}`);
+    }
+
     try {
       await next();
-      recordResponse();
-    } catch {
-      recordError();
-      recordResponse();
+    } catch (error) {
+      if (span) {
+        recordError(span);
+      }
+      throw error;
+    } finally {
+      if (span) {
+        recordResponse(ctx, span);
+        span.end();
+      }
     }
   };
 }
